f7b50ac36a997df3a59daeb4de64d6a54cdf345b
oyeks-ayo <adamsonia62@gmail.com>
2025-12-03 07:27:46 -0800

    fix(env): update DATABASE_URL to use a placeholder password; add PostgreSQL setup instructions

--- config/config.py
+++ config/config.py
@@ -7,3 +7,7 @@ class Appconfig(object):
     SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SECRET_KEY = os.getenv("SECRET_KEY", 'fallback_secret_key')
+
+# export PATH=$PATH:"/c/Program Files/PostgreSQL/18/bin" TO ADD POSTGRESQL TO PATH ON WINDOWS
+# $ psql -U postgres TO ACCESS POSTGRESQL TERMINAL
+# CREATE DATABASE ifarm_db; TO CREATE DATABASE IN POSTGRESQL TERMINAL
